87cd9951ebf65802ef48375843790ad92f2fbd42
Vedant K <vedantsk103@gmail.com>
2021-08-13 19:02:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import pickle
+import os
+from flask import  Flask, render_template, url_for, request, jsonify
+from flask_cors import cross_origin
+
+app = Flask(__name__, template_folder="template")
+model = pickle.load(open("./models/rf.pkl", "rb"))
+print("Model Loaded")
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    return render_template("predict.html")
+
+@app.route("/predict", methods=['GET', 'POST'])
+@cross_origin()
+def predict():
+    if request.method == "POST":
+        #variance
+        variance = request.form['variance']
+        #skewness
+        skewness = request.form['skewness']
+        #curtosis
+        curtosis = request.form['curtosis']
+        #entropy
+        entropy = request.form['entropy']
+
+        input_lst = [[variance, skewness, curtosis, entropy ]]
+        pred = model.predict(input_lst)
+        output = pred
+        if output == 0:
+            return render_template('predict.html', prediction_text = "This is an unauthenticated note!")
+        if output ==1:
+            return render_template('predict.html', prediction_text = "This is an authenticated note!")
+    return render_template("predict.html")    
+
+ 
+
+if __name__=='__main__':
+    port = os.environ.get("PORT", 5000)
+    app.run(debug=False, host="0.0.0.0" , port=port)
